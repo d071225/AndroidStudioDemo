efdb86ab1cd80faf8a9fbb0bed6814a115bd3a86
d071225 <764552635@qq.com>
2017-05-05 17:56:10 +0800

    添加主支3，修改gitfragment

--- androidfengkuangdemo/src/main/java/com/example/fragment/GitFragment.java
+++ androidfengkuangdemo/src/main/java/com/example/fragment/GitFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -17,6 +18,7 @@ public class GitFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         TextView textView;
+        ImageView imageView;
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
