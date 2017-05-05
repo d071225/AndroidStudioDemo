0754c67bc17ee0591e0d1c44183639f764b21622
d071225 <764552635@qq.com>
2017-05-05 17:41:36 +0800

    主支添加gitfragmnet

---
+++ androidfengkuangdemo/src/main/java/com/example/fragment/GitFragment.java
@@ -0,0 +1,20 @@
+package com.example.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by DY on 2017/5/5.
+ */
+
+public class GitFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
